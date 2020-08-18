adbdbb5e706760c5051d49c9d1f3a3c42837fe54
ahamdyshehata7@gmail.com <ahamdyshehata7@gmail.com>
2020-08-18 21:07:52 +0200

    edit cash okhttp

--- app/src/main/java/com/ashehata/bittask/di/modules/NetworkModule.kt
+++ app/src/main/java/com/ashehata/bittask/di/modules/NetworkModule.kt
@@ -35,36 +35,13 @@ class NetworkModule {
             .readTimeout(20, TimeUnit.SECONDS)
             .connectTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(20, TimeUnit.SECONDS)
-            // Specify the cache we created earlier.
             .cache(getCash(context))
             .addInterceptor { chain ->
-                // Get the request from the chain.
                 var request = chain.request()
-                /*
-                *  Leveraging the advantage of using Kotlin,
-                *  we initialize the request and change its header depending on whether
-                *  the device is connected to Internet or not.
-                */
                 request = if (isNetworkAvailable(context))
-                /*
-                *  If there is Internet, get the cache that was stored 5 seconds ago.
-                *  If the cache is older than 5 seconds, then discard it,
-                *  and indicate an error in fetching the response.
-                *  The 'max-age' attribute is responsible for this behavior.
-                */
                     request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
                 else
-                /*
-                *  If there is no Internet, get the cache that was stored 3 days ago.
-                *  If the cache is older than 7 days, then discard it,
-                *  and indicate an error in fetching the response.
-                *  The 'max-stale' attribute is responsible for this behavior.
-                *  The 'only-if-cached' attribute indicates to not retrieve new data; fetch the cache only instead.
-                */
                     request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 3).build()
-                // End of if-else statement
-
-                // Add the modified request to the chain.
                 chain.proceed(request)
             }
             .build()
